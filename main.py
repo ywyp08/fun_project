3db48967554b7a57c6e61472dfa73b7b662d3689
ywyp08 <stepa.spilka@gmail.com>
2024-01-26 14:08:28 +0100

    day saving function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 import tkinter as tk
-from tkinter import Checkbutton, IntVar
+from tkinter import Checkbutton, IntVar, messagebox
 
 def add_action():
     action_text = entry.get()
@@ -34,6 +35,15 @@ def open_manager():
     add_habit_button = tk.Button(manager_window, text="Add Habit", command=add_habit)
     add_habit_button.pack()
 
+def save_day():
+    day_date = datetime.today().strftime('%Y-%m-%d')
+    try:
+        with open("daily_activities.txt", "a") as file:
+            for var, text in actions_checkboxes:
+                file.write(f"{day_date}: {text}: {var.get()}\n")
+        messagebox.showinfo("Day Saved", "Data saved successfully!")
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred: {str(e)}")
 
 def read_habits_from_file():
     try:
@@ -51,18 +61,21 @@ entry.pack(pady=10)
 
 add_action_button = tk.Button(main_window, text="Add Action", command=add_action)
 add_action_button.pack()
+save_day_button = tk.Button(main_window, text="Save Day", command=save_day)
+save_day_button.pack()
 
 action_frame = tk.Frame(main_window)
 action_frame.pack(pady=10)
 
+actions_checkboxes = []
 habits = read_habits_from_file()
 for habit in habits:
-    habit_checkbox = Checkbutton(action_frame, text=habit)
+    var = IntVar()
+    habit_checkbox = Checkbutton(action_frame, text=habit, variable=var)
     habit_checkbox.pack(anchor=tk.W)
+    actions_checkboxes.append((var, habit))
 
 open_manager_button = tk.Button(main_window, text="Open Action Manager", command=open_manager)
 open_manager_button.pack()
-
-actions_checkboxes = []
 
 main_window.mainloop()
