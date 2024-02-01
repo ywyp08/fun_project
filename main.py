7582263789ff406999bba8bc225c3405a5da3f81
ywyp08 <stepa.spilka@gmail.com>
2024-02-01 04:42:33 +0100

    change of scope

--- main.py
+++ main.py
@@ -1,69 +1,38 @@
 import tkinter as tk
-from tkinter import Checkbutton, IntVar
-from data_manipulation import *
+from tkinter import messagebox
+from datetime import datetime
 
-def add_action():
-    action_text = entry.get()
+def add_to_do():
+    to_do_text = to_do_text_entry.get()
+    to_do_importance = to_do_importance_entry.get()
+    date = datetime.today().strftime('%Y-%m-%d')
 
-    if action_text:
-        var = IntVar()
-        action_checkbox = Checkbutton(frame, text=action_text, variable=var)
-        action_checkbox.pack(anchor=tk.W)
-        actions.append((var, action_text))
-        entry.delete(0, tk.END)
-
-def add_habit():
-    habit_text = habit_entry.get()
-    with open("habits.txt", "a") as file:
-        file.write(f"{habit_text}\n")
-    habit_entry.delete(0, tk.END)
-    var = IntVar()
-    action_checkbox = Checkbutton(frame, text=habit_text, variable=var)
-    action_checkbox.pack(anchor=tk.W)
-
-def open_manager():
-    global habit_entry
-    manager_window = tk.Toplevel(main_window)
-    manager_window.title("Action Manager")
-
-    habit_entry_label = tk.Label(manager_window, text="Enter Habit:")
-    habit_entry_label.pack()
-
-    habit_entry = tk.Entry(manager_window, width=30)
-    habit_entry.pack(pady=10)
-
-    add_habit_button = tk.Button(manager_window, text="Add Habit", command=add_habit)
-    add_habit_button.pack()
+    with open("to_dos.txt", "a") as file:
+        if to_do_text:
+            file.write(f"{date}: {to_do_text}: {to_do_importance}\n")
+            to_do_text_entry.delete(0, tk.END)
+            to_do_importance_entry.delete(0, tk.END)
+            messagebox.showinfo("Saved", "Data saved successfully!")
+        else:
+            messagebox.showerror("Error", "There is no to-do to be saved.")
 
 main_window = tk.Tk()
-main_window.title("Action List")
+main_window.title("To-do Manager")
+add_frame = tk.Frame(main_window)
+add_frame.pack(pady=10)
+manager_frame = tk.Frame(main_window)
+manager_frame.pack(pady=10)
 
-""" entry = tk.Entry(main_window, width=30)
-entry.pack(pady=10)
+tk.Label(add_frame, text="To-do").grid(row=0, column=0)
+tk.Label(add_frame, text="Importance").grid(row=0, column=1)
 
-add_action_button = tk.Button(main_window, text="Add Action", command=add_action)
-add_action_button.pack()
+to_do_text_entry = tk.Entry(add_frame, width=30)
+to_do_text_entry.grid(row=1, column=0)
+to_do_importance_entry = tk.Entry(add_frame, width=10)
+to_do_importance_entry.grid(row=1, column=1)
 
-save_day_button = tk.Button(main_window, text="Save Day", command=save_day)
-save_day_button.pack() """
+tk.Button(add_frame, text="Add to-do", command=add_to_do).grid(row=1, column=3)
 
-frame = tk.Frame(main_window)
-frame.pack(pady=10)
-
-tk.Label(frame, text="Actions").grid(row=0, column=0)
-tk.Label(frame, text="Goals").grid(row=0, column=1)
-
-actions = [] 
-habits = read_values_from_file("habits.txt")
-
-row = 1
-for habit in habits:
-    var = IntVar()
-    Checkbutton(frame, text=habit, variable=var).grid(row=row, column=0)
-    row += 1
-    actions.append((var, habit))
-
-open_manager_button = tk.Button(main_window, text="Goal Manager", command=open_manager)
-open_manager_button.pack()
+tk.Label(manager_frame, text="To-Dos").grid(row=0, column=0)
 
 main_window.mainloop()
