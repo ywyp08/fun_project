236f554aee6807063f388aad01efa652999b84e3
ywyp08 <stepa.spilka@gmail.com>
2024-02-02 17:56:59 +0100

    fixed bugs

--- main.py
+++ main.py
@@ -2,64 +2,69 @@ import tkinter as tk
 from tkinter import messagebox
 from datetime import datetime
 
-def add_to_do():
-    to_do_text = to_do_text_entry.get()
-    to_do_importance = to_do_importance_entry.get()
+def add_to_do(to_do_text):
+    to_do_text = text_entry.get()
     date = datetime.today().strftime('%Y-%m-%d')
 
     with open("to_dos.txt", "a") as file:
         if to_do_text:
-            file.write(f"{date}: {to_do_text}: {to_do_importance}: active\n")
-            to_do_text_entry.delete(0, tk.END)
-            to_do_importance_entry.delete(0, tk.END)
-            messagebox.showinfo("Saved", "Data saved successfully!")
+            file.write(f"{date}: {to_do_text}: 0: active: 0\n")
+            text_entry.delete(0, tk.END)
         else:
             messagebox.showerror("Error", "There is no to-do to be saved.")
-    display_to_dos()  # Update the to-do list display
+    display_to_dos()
 
 def display_to_dos():
     for widget in manager_frame.winfo_children():
         widget.destroy()
-
-    tk.Label(manager_frame, text="To-Do").grid(row=0, column=0)
-    tk.Label(manager_frame, text="Importance").grid(row=0, column=1, columnspan=3)
-    tk.Label(manager_frame, text="Finish").grid(row=0, column=4, columnspan=2)
-
     with open("to_dos.txt", "r") as file:
         lines = file.readlines()
         sorted_lines = sorted(lines, key=lambda x: int(x.split(": ")[2]), reverse=True)
-        for i, line in enumerate(sorted_lines, start=1):
-            date, to_do_text, to_do_importance, status = line.strip().split(": ")
-            tk.Label(manager_frame, text=to_do_text).grid(row=i, column=0)
-            tk.Label(manager_frame, text=to_do_importance).grid(row=i, column=1)
-            tk.Button(manager_frame, text="+", command= lambda i=i: importance_plus(i)).grid(row=i, column=2) 
-            tk.Button(manager_frame, text="-", command= lambda i=i: importance_minus(i)).grid(row=i, column=3)
-            tk.Button(manager_frame, text="done").grid(row=i, column=4)
-            tk.Button(manager_frame, text="delete").grid(row=i, column=5)
+    i = 0
+    tk.Label(manager_frame, text="To-Do").grid(row=i, column=0)
+    tk.Label(manager_frame, text="Importance").grid(row=i, column=1, columnspan=3)
+    tk.Label(manager_frame, text="Finish").grid(row=i, column=4, columnspan=2)
+    for line in sorted_lines:
+        i += 1
+        line_list = [x.strip() for x in line.split(':')]
+        if line_list[3] == "active":
+            tk.Label(manager_frame, text=line_list[1]).grid(row=i, column=0)
+            tk.Label(manager_frame, text=line_list[2]).grid(row=i, column=1)
+            tk.Button(manager_frame, text="+", command= lambda i=i, sign=1: update_importance(i, sign)).grid(row=i, column=2) 
+            tk.Button(manager_frame, text="-", command= lambda i=i, sign=-1: update_importance(i, sign)).grid(row=i, column=3)
+            tk.Button(manager_frame, text="done", command= lambda i=i: done_to_do(i)).grid(row=i, column=4)
+            tk.Button(manager_frame, text="delete", command= lambda i=i: delete_to_do(i)).grid(row=i, column=5)
 
-def importance_plus(index):
-    update_importance(index, 1)
-
-def importance_minus(index):
-    update_importance(index, -1)
-
-def update_importance(index, change):
-    # Read existing to-dos
+def update(index, importance, state):
     with open("to_dos.txt", "r") as file:
         lines = file.readlines()
-
-    # Update the importance of the specific to-do
-    date, to_do_text, to_do_importance, state = lines[index - 1].strip().split(": ")
-    new_importance = max(int(to_do_importance) + change, 0)
-    lines[index - 1] = f"{date}: {to_do_text}: {new_importance}: {state}\n"
-
-    # Write the updated to-dos back to the file
+        sorted_lines = sorted(lines, key=lambda x: int(x.split(": ")[2]), reverse=True)
+    line_list = [x.strip() for x in sorted_lines[index - 1].split(':')]
+    if (importance == 1 or importance == -1):
+        new_importance = int(line_list[2]) + importance
+        sorted_lines[index - 1] = f"{line_list[0]}: {line_list[1]}: {new_importance}: {line_list[3]}: {line_list[4]}\n"
+    elif state == "done":
+        new_date = datetime.today().strftime('%Y-%m-%d')
+        sorted_lines[index - 1] = f"{line_list[0]}: {line_list[1]}: -999: done: {new_date}\n"
+    elif state == "delete":
+        new_date = datetime.today().strftime('%Y-%m-%d')
+        sorted_lines[index - 1] = f"{line_list[0]}: {line_list[1]}: -999: deleted: {new_date}\n"
     with open("to_dos.txt", "w") as file:
-        file.writelines(lines)
+        file.writelines(sorted_lines)
 
-    # Update the display
+def update_importance(index, change):
+    update(index=index, importance=change, state=0)
     display_to_dos()
 
+def delete_to_do(index):
+    update(index=index, importance=0, state="delete")
+    display_to_dos()
+
+def done_to_do(index):
+    update(index=index, importance=0, state="done")
+    display_to_dos()
+
+# Main window and it's properties
 main_window = tk.Tk()
 main_window.title("To-do Manager")
 
@@ -68,14 +73,11 @@ add_frame.pack(pady=10)
 manager_frame = tk.Frame(main_window)
 manager_frame.pack(pady=10)
 
-# filling the add_frame
 tk.Label(add_frame, text="To-do").grid(row=0, column=0)
-tk.Label(add_frame, text="Importance").grid(row=0, column=1)
-to_do_text_entry = tk.Entry(add_frame, width=30)
-to_do_text_entry.grid(row=1, column=0)
-to_do_importance_entry = tk.Entry(add_frame, width=10)
-to_do_importance_entry.grid(row=1, column=1)
-tk.Button(add_frame, text="Add", command=add_to_do).grid(row=1, column=3)
+text_entry = tk.Entry(add_frame, width=30)
+text_entry.grid(row=1, column=0)
+text_entry.bind('<Return>', add_to_do)
+tk.Button(add_frame, text="Add", command=lambda: add_to_do(text_entry.get())).grid(row=1, column=2)
 
 display_to_dos()
 
